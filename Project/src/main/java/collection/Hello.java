da9402398dbb047e3e486b44dda2f64a8305d20d
SANEETASEBASTIAN <saneetasebastian@gmail.com>
2024-03-11 21:12:38 +0530

    Connect

---
+++ Project/src/main/java/collection/Hello.java
@@ -0,0 +1,10 @@
+package collection;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello welcome to github");
+	}
+
+}
